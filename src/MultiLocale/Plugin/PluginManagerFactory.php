71b3884c93b8fae3aa1ae445f96c41dc6b7b4167
Toan LE Macbook <toan@visualweber.com>
2016-10-16 01:00:49 +0700

    Update core

---
+++ src/MultiLocale/Plugin/PluginManagerFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace MultiLocale\Plugin;
+
+use Zend\Mvc\Service\AbstractPluginManagerFactory;
+
+class PluginManagerFactory extends AbstractPluginManagerFactory {
+
+    const PLUGIN_MANAGER_CLASS = 'MultiLocale\Plugin\PluginManager';
+
+}
